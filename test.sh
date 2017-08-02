80d7d1eeaf5053f73e7837063266daf6b41d8cd9
Jean Niklas L'orange <jeannikl@hypirion.com>
2017-08-02 22:37:20 +0200

    Add go fmt to ci

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 if ! hash golint 2>/dev/null; then
     go get github.com/golang/lint/golint
@@ -9,6 +9,20 @@ if ! hash megacheck 2>/dev/null; then
     go get honnef.co/go/tools/cmd/megacheck
 fi
 
+echo + go fmt
+gofiles="$(find . -type f -iname '*.go')"
+if [ -n "$gofiles" ]; then
+    unformatted="$(gofmt -l $gofiles)"
+    if [ -n "$unformatted" ]; then
+        echo >&2 "Go files must be formatted with gofmt. Please run:"
+        for fn in $unformatted; do
+            echo >&2 "  gofmt -w $PWD/$fn"
+        done
+        exit 1
+    fi
+fi
+
+set -x
 go vet ./...
 golint ./...
 megacheck ./...
